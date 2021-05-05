66d8352a6e75132ea4e86a1305851ffc8f82157d
Arkadiusz Zygan <a.zygan@gmail.com>
2021-05-05 17:38:51 +0200

    Add losing logic

--- src/App.js
+++ src/App.js
@@ -5,14 +5,42 @@ import Quiz from "./components/Quiz";
 
 function App() {
   const [questionNumber, setQuestionNumber] = useState(1);
+  const [end, setEnd] = useState(false);
+  const [score, setScore] = useState(0);
+
+  const showScore = () => {
+    if (score === 1) {
+      return " punkt";
+    } else if ((score === 2, 3, 4)) {
+      return " punkty";
+    } else {
+      return " punktów";
+    }
+  };
+  console.log(showScore());
   return (
     <div className='app'>
       <div className='top'>front-quiz</div>
-      <Quiz
-        data={data}
-        questionNumber={questionNumber}
-        setQuestionNumber={setQuestionNumber}
-      />
+      {end ? (
+        <>
+          <div className='endText'>
+            <h1>Przegrałeś :(</h1>
+            <h3>
+              Zdobyłeś {score}
+              {showScore()}!
+            </h3>
+          </div>
+        </>
+      ) : (
+        <Quiz
+          data={data}
+          questionNumber={questionNumber}
+          setQuestionNumber={setQuestionNumber}
+          setEnd={setEnd}
+          score={score}
+          setScore={setScore}
+        />
+      )}
     </div>
   );
 }
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,6 +1,13 @@
 import { useState, useEffect } from "react";
 
-const Quiz = ({ data, questionNumber, setQuestionNumber }) => {
+const Quiz = ({
+  data,
+  questionNumber,
+  setQuestionNumber,
+  setEnd,
+  score,
+  setScore,
+}) => {
   const [question, setQuestion] = useState(null);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [className, setClassName] = useState("answer");
@@ -9,22 +16,39 @@ const Quiz = ({ data, questionNumber, setQuestionNumber }) => {
     setQuestion(data[questionNumber - 1]);
   }, [data, questionNumber]);
 
+  const delay = (duration, callback) => {
+    setTimeout(() => {
+      callback();
+    }, duration);
+  };
+
   const handleClick = (ans) => {
     setSelectedAnswer(ans);
     setClassName("answer active");
-    setTimeout(() => {
-      setClassName(ans.correct ? "answer correct" : "answer wrong");
-    }, 1500);
+    delay(1500, () =>
+      setClassName(ans.correct ? "answer correct" : "answer wrong")
+    );
+    delay(4500, () => {
+      if (ans.correct) {
+        setQuestionNumber((prev) => prev + 1);
+        setScore((prev) => prev + 1);
+        setSelectedAnswer(null);
+      } else {
+        setEnd(true);
+      }
+    });
   };
 
+  console.log(score);
   return (
     <div className='quiz'>
       <div className='question'>{question?.question}</div>
       <div className='answers'>
-        {question?.answers.map((ans) => (
+        {question?.answers.map((ans, index) => (
           <div
             className={selectedAnswer === ans ? className : "answer"}
             onClick={() => handleClick(ans)}
+            key={index}
           >
             {ans.text}
           </div>
